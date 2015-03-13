5e7f7165302ec4714a0986e4c1f3991411ea26c1
Shankar Dhanasekaran <shankar@opendrops.com>
2015-03-13 12:39:56 +0530

    Changes to default mix project files by phoenix

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-Testapp
-=======
+# Testapp
 
-** TODO: Add description **
+To start your new Phoenix application:
+
+1. Install dependencies with `mix deps.get`
+2. Start Phoenix endpoint with `mix phoenix.server`
+
+Now you can visit `localhost:4000` from your browser.
--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule Testapp.Mixfile do
     [app: :testapp,
      version: "0.0.1",
      elixir: "~> 1.0",
+     elixirc_paths: ["lib", "web"],
+     compilers: [:phoenix] ++ Mix.compilers,
      deps: deps]
   end
 
@@ -12,19 +14,17 @@ defmodule Testapp.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [mod: {Testapp, []},
+     applications: [:phoenix, :cowboy, :logger]]
   end
 
-  # Dependencies can be Hex packages:
+  # Specifies your project dependencies
   #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
+  # Type `mix help deps` for examples and options
   defp deps do
-    []
+    [{:phoenix, github: "phoenixframework/phoenix", override: true},
+     {:phoenix_ecto, "~> 0.1"},
+     {:postgrex, ">= 0.0.0"},
+     {:cowboy, "~> 1.0"}]
   end
 end
